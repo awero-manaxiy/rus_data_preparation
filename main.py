b28ac4b89d29418a238f9671f4d5805a149be7bf
roman <roma-inin@mail.ru>
2023-11-29 14:21:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+names = [
+    'avg_pr_housing_price',
+    'avg_sec_housing_price',
+    'inflation_total',
+    'inflation_utility',
+    'income_prc',
+    'avg_income',
+    'company_wage',
+    'built_houses',
+    'mortgage_prc',
+    'unemployment_rate',
+    'num_people',
+    'urbanization',
+    'pop_growth',
+    'grp_prc',
+    'grp_total',
+    'grp_per_capita',
+    'real_income',
+]
+
+months = {'январь': 1, 'февраль': 2, 'март': 3, 'апрель': 4,
+          'май': 5, 'июнь': 6, 'июль': 7, 'август': 8, 'сентябрь': 9,
+          'октябрь': 10, 'ноябрь': 11, 'декабрь': 12, 'авуст': 8}
+
+quarters = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'год': 'FULL'}
+
+num_variables = 17
+
+df_fin = None
+
+for i in range(num_variables):
+    df = pd.read_excel(f'Показатели/excel ({i}).xlsx')
+    df = df.rename(columns={'Unnamed: 0': 'region'})
+
+    if all(df.columns[1:].str.isnumeric()):
+        cols = {x: f'{names[i]}_{x}' for x in df.columns[1:]}
+        df = df.rename(columns=cols)
+
+    elif any(df.columns[1:].str.contains('квартал')):
+        cols = {x: f'{names[i]}_quarter_{quarters[x.split()[1]]}_{x.split()[0].strip(",")}' for x in df.columns[1:]}
+        df = df.rename(columns=cols)
+
+    elif not any(df.columns[1:].str.contains('-')):
+        cols = {x: f'{names[i]}_month_{months[x.split()[1]]}_{x.split()[0]}' for x in df.columns[1:]}
+        df = df.rename(columns=cols)
+
+    else:
+        cols = {x: f'{names[i]}_month_{months[x.split()[-1]]}_{x.split()[0].strip(",")}' for x in df.columns[1:]}
+        df = df.rename(columns=cols)
+
+    if i == 0:
+        df_fin = df
+    else:
+        df_fin = df_fin.merge(df, on='region', how='outer')
+
+df_fin.to_csv('initial_data.csv')
+
